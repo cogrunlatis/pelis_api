a83daa47fb6367196434f1e112b6ef87a6bd0a93
Maximiliano Luci <maximiliano.luci@icloud.com>
2022-11-21 16:43:35 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Peliculas TMDB',
+      title: 'Actores TMDB',
       initialRoute: '/',
       routes: {
         '/': (BuildContext context) => HomePage(),
